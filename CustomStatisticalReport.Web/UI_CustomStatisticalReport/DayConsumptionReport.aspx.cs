eb1cb2a6af9da12297ea2d327ba32dcbbf63e4b7
sondacal <673808441@qq.com>
2016-07-26 16:49:13 +0800

    综合统计报表

---
+++ CustomStatisticalReport.Web/UI_CustomStatisticalReport/DayConsumptionReport.aspx.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.Services;
+using System.Data;
+using CustomStatisticalReport.Service.CustomDayConsumptionReport;
+namespace CustomStatisticalReport.Web.UI_CustomStatisticalReport
+{
+    public partial class DayConsumptionReport : WebStyleBaseForEnergy.webStyleBase
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            base.InitComponts();
+            ////////////////////调试用,自定义的数据授权
+#if DEBUG
+            List<string> m_DataValidIdItems = new List<string>() { "zc_nxjc_qtx","zc_nxjc_byc" };
+            AddDataValidIdGroup("ProductionOrganization", m_DataValidIdItems);
+#elif RELEASE
+#endif
+            this.OrganisationTree_ProductionLine.Organizations = GetDataValidIdGroup("ProductionOrganization");                          //向web用户控件传递数据授权参数
+            this.OrganisationTree_ProductionLine.PageName = "DayConsumptionReport.aspx";                     //向web用户控件传递当前调用的页面名称
+            this.OrganisationTree_ProductionLine.LeveDepth = 5;
+
+
+
+            ///以下是接收js脚本中post过来的参数
+            string m_FunctionName = Request.Form["myFunctionName"] == null ? "" : Request.Form["myFunctionName"].ToString();             //方法名称,调用后台不同的方法
+            string m_Parameter1 = Request.Form["myParameter1"] == null ? "" : Request.Form["myParameter1"].ToString();                   //方法的参数名称1
+            string m_Parameter2 = Request.Form["myParameter2"] == null ? "" : Request.Form["myParameter2"].ToString();                   //方法的参数名称2
+            if (m_FunctionName == "ExcelStream")
+            {
+                //ExportFile("xls", "导出报表1.xls");
+                string m_ExportTable = m_Parameter1.Replace("&lt;", "<");
+                m_ExportTable = m_ExportTable.Replace("&gt;", ">");
+                DayConsumptionReportService.ExportExcelFile("xls", m_Parameter2 + "能耗日报.xls", m_ExportTable);
+            }
+            if (!IsPostBack)
+            {
+
+            }
+        }
+        /// <summary>
+        /// 获得报表数据并转换为json
+        /// </summary>
+        /// <returns>column的json字符串</returns>
+        [WebMethod]
+        public static string GetReportData(string organizationId, string datetime)
+        {
+            DataTable m_ReportData = CustomStatisticalReport.Service.CustomDayConsumptionReport.DayConsumptionReportService.GetReportData(organizationId, datetime);
+            string m_ReportDataJson = EasyUIJsonParser.TreeGridJsonParser.DataTableToJsonByLevelCode(m_ReportData,"LevelCode");
+            return m_ReportDataJson;
+        }
+
+        /// <summary>
+        /// 获得报表数据并转换为json
+        /// </summary>
+        /// <returns>column的json字符串</returns>
+        [WebMethod]
+        public static string PrintFile()
+        {
+            //string[] m_TagData = new string[] { "10月份", "报表类型:日报表", "汇总人:某某某", "审批人:某某某" };
+            //string m_HtmlData = StatisticalReportHelper.CreatePrintHtmlTable(mFileRootPath +
+            //    REPORT_TEMPLATE_PATH, myDataTable, m_TagData);
+            //return m_HtmlData;
+            return "";
+        }
+    }
+}
---
+++ CustomStatisticalReport.Web/obj/Debug/Package/PackageTmp/UI_CustomStatisticalReport/ProductionDailyReport/js/DailyProduction.js
@@ -0,0 +1,168 @@
+﻿$(function () {
+    Loadtreegrid("first", { "rows": [], "total": 0 });
+    InitDate();
+
+});
+//初始化日期框
+function InitDate() {
+    var nowDate = new Date();
+    nowDate.setDate(nowDate.getDate() - 1);
+    var dateString = nowDate.getFullYear() + '-' + (nowDate.getMonth());
+    $('#DatetimeF').datebox('setValue', dateString);
+}
+function onOrganisationTreeClick(myNode) {
+    $('#OrganizationIdF').attr('value', myNode.OrganizationId);  //textbox('setText', myNode.OrganizationId);
+    $('#OrganizationNameF').textbox('setText', myNode.text);
+}
+function Loadtreegrid(type, myData) {
+    if (type == "first") {
+        $('#DailyProduction').treegrid({
+            idField: 'id',
+            treeField: 'text',
+            rownumbers: true,
+            singleSelect: true,
+            toolbar: '#toolbar_head',
+            data: myData,
+            fit: true,
+            frozenColumns: [[
+                { field: 'id', title: 'Id', width: 110, align: 'center', hidden:true },
+                { field: 'text', title: '名称', width: 130, align: 'center' }
+            ]],
+            columns: [[
+                 { title: '月计划', colspan: 4, width: 280, align: 'center' },
+                 { title: '日计', colspan: 4, width: 280, align: 'center' },
+                 { title: '月合计', colspan: 4, width: 280, align: 'center' },
+                 { title: '年累计', colspan: 4, width: 280, align: 'center' }
+            ], [
+                { field: 'Output_Plan', title: '产量（t）', width: 70, align: 'center' },
+                { field: 'RunTime_Plan', title: '运行时间(h)', width: 70, align: 'center' },
+                { field: 'TimeOutput_Plan', title: '台时(t/h)', width: 70, align: 'center' },
+                { field: 'RunRate_Plan', title: '运转率(%)', width: 70, align: 'center' },
+                { field: 'Output_Day', title: '产量（t）', width: 70, align: 'center' },
+                { field: 'RunTime_Day', title: '运行时间(h)', width: 70, align: 'center' },
+                { field: 'TimeOutput_Day', title: '台时(t/h)', width: 70, align: 'center' },
+                { field: 'RunRate_Day', title: '运转率(%)', width: 70, align: 'center' },
+                { field: 'Output_Month', title: '产量（t）', width: 70, align: 'center' },
+                { field: 'RunTime_Month', title: '运行时间(h)', width: 70, align: 'center' },
+                { field: 'TimeOutput_Month', title: '台时(t/h)', width: 70, align: 'center' },
+                { field: 'RunRate_Month', title: '运转率(%)', width: 70, align: 'center' },
+                { field: 'Output_Year', title: '产量（t）', width: 70, align: 'center' },
+                { field: 'RunTime_Year', title: '运行时间(h)', width: 70, align: 'center' },
+                { field: 'TimeOutput_Year', title: '台时(t/h)', width: 70, align: 'center' },
+                { field: 'RunRate_Year', title: '运转率(%)', width: 70, align: 'center' }
+            ]]
+        });
+    }
+}
+
+function QueryReportFun() {
+    var m_QueryDate = $('#DatetimeF').datebox('getValue');
+    var m_OrganizationId = $('#OrganizationIdF').val();
+    $.ajax({
+        type: "POST",
+        url: "DailyProduction.aspx/GetDailyProductionData",
+        data: '{myOrganizationId: "' + m_OrganizationId + '", myDateTime: "' + m_QueryDate + '"}',
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            var m_MsgData = jQuery.parseJSON(msg.d);
+            if (m_MsgData != null && m_MsgData != undefined) {
+                for (var i = 0; i < m_MsgData.Length; i++) {
+                    var m_Output_Plan = 0.0;
+                    var m_RunTime_Plan = 0.0;
+                    var m_TimeOutputD_Plan = 0.0;
+                    var m_RunRateD_Plan = 0.0;
+                    var m_Output_Day = 0.0;
+                    var m_RunTime_Day = 0.0;
+                    var m_TimeOutputD_Day = 0.0;
+                    var m_RunRateD_Day = 0.0;
+                    var m_Output_Month = 0.0;
+                    var m_RunTime_Month = 0.0;
+                    var m_TimeOutputD_Month = 0.0;
+                    var m_RunRateD_Month = 0.0;
+                    var m_Output_Year = 0.0;
+                    var m_RunTime_Year = 0.0;
+                    var m_TimeOutputD_Year = 0.0;
+                    var m_RunRateD_Year = 0.0;
+                    for (var j = 0; j < m_MsgData[i].chieldren.Length; j++) {
+                        /////////计划///////////
+                        m_Output_Plan = m_Output_Plan + m_MsgData[i].chieldren[j].Output_Plan;
+                        m_RunTime_Plan = m_RunTime_Plan + m_MsgData[i].chieldren[j].RunTime_Plan;
+                        if (m_TimeOutputD_Plan != 0) {
+                            m_TimeOutputD_Plan = m_TimeOutputD_Plan + m_MsgData[i].chieldren[j].Output_Plan / m_MsgData[i].chieldren[j].m_TimeOutput_Plan;
+                        }
+                        if (m_RunRateD_Plan != 0) {
+                            m_RunRateD_Plan = m_RunRateD_Plan + m_MsgData[i].chieldren[j].m_RunTime_Plan / m_MsgData[i].chieldren[j].m_RunRate_Plan;
+                        }
+                        /////////日统计/////////
+                        m_Output_Day = m_Output_Day + m_MsgData[i].chieldren[j].Output_Day;
+                        m_RunTime_Day = m_RunTime_Day + m_MsgData[i].chieldren[j].RunTime_Day;
+                        if (m_TimeOutputD_Day != 0) {
+                            m_TimeOutputD_Day = m_TimeOutputD_Day + m_MsgData[i].chieldren[j].Output_Day / m_MsgData[i].chieldren[j].m_TimeOutput_Day;
+                        }
+                        if (m_RunRateD_Day != 0) {
+                            m_RunRateD_Day = m_RunRateD_Day + m_MsgData[i].chieldren[j].m_RunTime_Day / m_MsgData[i].chieldren[j].m_RunRate_Day;
+                        }
+                        /////////月统计/////////
+                        m_Output_Month = m_Output_Month + m_MsgData[i].chieldren[j].Output_Month;
+                        m_RunTime_Month = m_RunTime_Month + m_MsgData[i].chieldren[j].RunTime_Month;
+                        if (m_TimeOutputD_Month != 0) {
+                            m_TimeOutputD_Month = m_TimeOutputD_Month + m_MsgData[i].chieldren[j].Output_Month / m_MsgData[i].chieldren[j].m_TimeOutput_Month;
+                        }
+                        if (m_RunRateD_Month != 0) {
+                            m_RunRateD_Month = m_RunRateD_Month + m_MsgData[i].chieldren[j].m_RunTime_Month / m_MsgData[i].chieldren[j].m_RunRate_Month;
+                        }
+                        /////////年统计/////////
+                        m_Output_Year = m_Output_Year + m_MsgData[i].chieldren[j].Output_Year;
+                        m_RunTime_Year = m_RunTime_Year + m_MsgData[i].chieldren[j].RunTime_Year;
+                        if (m_TimeOutputD_Year != 0) {
+                            m_TimeOutputD_Year = m_TimeOutputD_Year + m_MsgData[i].chieldren[j].Output_Year / m_MsgData[i].chieldren[j].m_TimeOutput_Year;
+                        }
+                        if (m_RunRateD_Year != 0) {
+                            m_RunRateD_Year = m_RunRateD_Year + m_MsgData[i].chieldren[j].m_RunTime_Year / m_MsgData[i].chieldren[j].m_RunRate_Year;
+                        }
+                    }
+                    ////////////计划/////////////
+                    m_MsgData[i].Output_Plan = m_Output_Plan;
+                    m_MsgData[i].RunTime_Plan = m_RunTime_Plan;
+                    if(m_TimeOutputD_Plan != 0)
+                    {
+                        m_MsgData[i].m_TimeOutput_Plan = m_Output_Plan / m_TimeOutputD_Plan;
+                    }
+                    if (m_RunRateD_Plan != 0) {
+                        m_MsgData[i].m_RunRate_Plan = m_Output_Plan / m_RunRateD_Plan;
+                    }
+                    ////////////日统计/////////////
+                    m_MsgData[i].Output_Day = m_Output_Day;
+                    m_MsgData[i].RunTime_Day = m_RunTime_Day;
+                    if (m_TimeOutputD_Day != 0) {
+                        m_MsgData[i].m_TimeOutput_Day = m_Output_Day / m_TimeOutputD_Day;
+                    }
+                    if (m_RunRateD_Day != 0) {
+                        m_MsgData[i].m_RunRate_Day = m_Output_Day / m_RunRateD_Day;
+                    }
+                    ////////////日统计/////////////
+                    m_MsgData[i].Output_Month = m_Output_Month;
+                    m_MsgData[i].RunTime_Month = m_RunTime_Month;
+                    if (m_TimeOutputD_Month != 0) {
+                        m_MsgData[i].m_TimeOutput_Month = m_Output_Month / m_TimeOutputD_Month;
+                    }
+                    if (m_RunRateD_Month != 0) {
+                        m_MsgData[i].m_RunRate_Month = m_Output_Month / m_RunRateD_Month;
+                    }
+                    ////////////日统计/////////////
+                    m_MsgData[i].Output_Year = m_Output_Year;
+                    m_MsgData[i].RunTime_Year = m_RunTime_Year;
+                    if (m_TimeOutputD_Year != 0) {
+                        m_MsgData[i].m_TimeOutput_Year = m_Output_Year / m_TimeOutputD_Year;
+                    }
+                    if (m_RunRateD_Year != 0) {
+                        m_MsgData[i].m_RunRate_Year = m_Output_Year / m_RunRateD_Year;
+                    }
+                }
+            }
+            $('#DailyProduction').treegrid("loadData", m_MsgData);
+            $('#DailyProduction').treegrid("collapseAll");
+        }
+    });
+}
